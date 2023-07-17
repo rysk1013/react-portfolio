8784c2e013bef1680ae61aa1336bbf712b8c9812
rysk1013 <0829ryosuke@gmail.com>
2023-07-17 19:18:40 +0900

    create components header homePage blogPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogPage from './components/BlogPage';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/blog' element={<BlogPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
